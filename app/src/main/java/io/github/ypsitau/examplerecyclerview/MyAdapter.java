04f4ee4ab36413b646cf8299779cc4687172a9ab
ypsitau <ypsitau@nifty.com>
2018-05-05 21:34:35 +0900

    implementing RecyclerView

--- app/src/main/java/io/github/ypsitau/examplerecyclerview/MyAdapter.java
+++ app/src/main/java/io/github/ypsitau/examplerecyclerview/MyAdapter.java
@@ -7,8 +7,6 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
-import java.util.List;
-
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 	private String[] dataSet;
 	public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
