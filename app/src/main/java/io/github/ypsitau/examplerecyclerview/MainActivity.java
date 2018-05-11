5229505203517251282a96eb16abf59811dc0f06
ypsitau <ypsitau@nifty.com>
2018-05-11 12:08:40 +0900

    added TabLayout

--- app/src/main/java/io/github/ypsitau/examplerecyclerview/MainActivity.java
+++ app/src/main/java/io/github/ypsitau/examplerecyclerview/MainActivity.java
@@ -1,26 +1,27 @@
+// The "implementation" statement required to be added in build.grade with "com.android.support:design:27.1.1"
+// to use TabLayout view.
+
 package io.github.ypsitau.examplerecyclerview;
 
-import android.content.Context;
-import android.content.Intent;
-import android.content.pm.ActivityInfo;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 	static FragmentFactory[] fragmentFactories = new FragmentFactory[] {
-		new PageAFragment.Factory(),
-		new PageBFragment.Factory(),
+			new PageAFragment.Factory(),
+			new PageBFragment.Factory(),
 	};
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -33,14 +34,25 @@ public class MainActivity extends AppCompatActivity {
 			public Fragment getItem(int position) { return fragmentFactories[position].create(); }
 			@Override
 			public int getCount() { return fragmentFactories.length; }
+
+			@Nullable
+			@Override
+			public CharSequence getPageTitle(int position) {
+				return getString(fragmentFactories[position].resIdTitle);
+			}
 		});
+		TabLayout tabLayout = findViewById(R.id.tabLayout);
+		tabLayout.setupWithViewPager(viewPager);
 		Util.init(this, (EditText) findViewById(R.id.editText_log));
 	}
 	public static abstract class FragmentFactory {
+		public final int resIdTitle;
+		public FragmentFactory(int resIdTitle) { this.resIdTitle = resIdTitle; }
 		abstract Fragment create();
 	}
 	public static class PageAFragment extends Fragment {
 		public static class Factory extends FragmentFactory {
+			Factory() { super(R.string.One_column); }
 			@Override
 			Fragment create() { return new PageAFragment(); }
 		}
@@ -56,6 +68,7 @@ public class MainActivity extends AppCompatActivity {
 	};
 	public static class PageBFragment extends Fragment {
 		public static class Factory extends FragmentFactory {
+			Factory() { super(R.string.Two_columns); }
 			@Override
 			Fragment create() { return new PageBFragment(); }
 		}
